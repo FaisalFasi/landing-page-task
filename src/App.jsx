69fbffb87571a9e2217ab5f175e277d4692d57d3
faisalrehman <faisalfasi18@gmail.com>
2023-08-24 11:39:43 +0200

    fixed case senstive bug for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import UberUns from "./pages/UberUns";
-import "./app.css";
+import "./App.css";
 
 function App() {
   return (
